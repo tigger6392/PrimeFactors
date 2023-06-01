40c676ffee1fb8caa48da90f821225ed8142be4e
tw2013.kim <tigger6392@gmail.com>
2023-06-01 15:35:02 +0900

    [Green-p37] tdd
    Signed-off-by: tw2013.kim <tigger6392@gmail.com>

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -6,3 +6,9 @@ TEST(PrimeFactors, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, p.of(1));
 }
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor p;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, p.of(2));
+}
